f096130bc95637d6b040550cb25b13e87ad02c3b
bocian67 <wladi.schneider13@googlemail.com>
2018-06-25 20:09:50 +0200

    change telephone number

--- events/views.py
+++ events/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
+from wagtail.core.models import Page
+from django.http import JsonResponse
+import json
+# TODO: Fix JSON
+def getEvents(request):
+    events = {"events": {}}
+    events_page = Page.objects.get(title="Kalender").get_children().live()
+    for item in events_page:
+        events["events"][str(item.specific.title)] = {'start_time': str(item.specific.time_from),
+                                       'end_time':str(item.specific.time_to), 
+                                       'description':str(item.specific.details)}
+    return JsonResponse(json.dumps(events), safe=False)
